580b3d475b5e40c49712e090603150d3b8db8add
Jamie Degnan <jamie.degnan@gmail.com>
2017-10-22 17:35:13 -0700

    Add a tagline and little extra space around the description

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,15 +4,16 @@ class HomeContent extends Component {
   render(){
     return(<div className="content-container card">
       <div className="home-content">
-      <h2>Home</h2>
-      <p className="bold">Where is home base?</p>
-      <p>I call Seattle, Washington, home -- but in the past I've been able to call my home California, Colorado, Paris, and Osaka.</p>
-      <p className="bold">What are you hoping to work in after DBC?</p>
-      <p>I'd like to pursue a career in Front End or Full Stack development :)</p>
-      <p className="bold">What are your top 5 yet to be traveled to destinations?</p>
-      <p>Norway, Iceland, Greece, Italy, Sweden.</p>
-        <p>I've been drawn towards are my whole life.  As a child I'd spend hours thinking up the perfect design or idea and through high school I took extra classes after school at another campus so that I could learn to silkscreen.  When I finally got to college I abandoned anything artistic as a possible pathway to make a living.  A few years back I found myself being pulled back towards what I really loved because website design seemed not only a possible career, but got me excited about art again.  I like to pay attention to the small details in design and can't wait to put my touches on more sites.</p>
-      </div>
+        <h2>Home</h2>
+        <p className="tagline">Hi!  My name is Jamie, I'm an aspiring web developer hoping to one day make the web a little nicer looking!</p>
+        <p className="bold">Where is home base?</p>
+        <p>I call Seattle, Washington, home -- but in the past I've been able to call my home California, Colorado, Paris, and Osaka.</p>
+        <p className="bold">What are you hoping to work in after DBC?</p>
+        <p>I'd like to pursue a career in Front End or Full Stack development :)</p>
+        <p className="bold">What are your top 5 yet to be traveled to destinations?</p>
+        <p>Norway, Iceland, Greece, Italy, Sweden.</p>
+        <p className="home-description">I've been drawn towards are my whole life.  As a child I'd spend hours thinking up the perfect design or idea and through high school I took extra classes after school at another campus so that I could learn to silkscreen.  When I finally got to college I abandoned anything artistic as a possible pathway to make a living.  A few years back I found myself being pulled back towards what I really loved because website design seemed not only a possible career, but got me excited about art again.  I like to pay attention to the small details in design and can't wait to put my touches on more sites.</p>
+        </div>
       </div>
 
     )
